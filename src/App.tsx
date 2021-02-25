f8f985b5606ee23055897dbac761e63aa2637541
olliswe <oliveriyer@gmail.com>
2021-02-25 22:00:30 +0100

    googla analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, useLocation } from "react-router-dom";
+import { Router, Route, Switch, useLocation } from "react-router-dom";
 import SongsContainer from "./components/SongsContainer";
 import styled, { ThemeProvider } from "styled-components";
 import { theme } from "helpers/theme";
@@ -8,6 +8,18 @@ import { QueryParamProvider } from "use-query-params";
 import { items } from "helpers/items";
 import Home from "components/Home";
 import { animated, useTransition } from "react-spring";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
+
+const trackingId = "UA-151060636-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+
+const history = createBrowserHistory();
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 const EmptyImage = styled.div`
   height: 0;
@@ -18,11 +30,11 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <BrowserRouter>
+      <Router history={history}>
         <QueryParamProvider ReactRouterRoute={Route}>
           <MainRouter />
         </QueryParamProvider>
-      </BrowserRouter>
+      </Router>
       {items.map((item, index) => (
         <>
           <EmptyImage key={index} style={{ backgroundImage: item.css }} />
